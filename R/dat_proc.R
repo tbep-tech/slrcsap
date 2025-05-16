a6d8c9067f8a787379663faafca0fe07a7fff78b
fawda123 <mbafs2012@gmail.com>
2025-05-16 07:36:56 -0400

    added slr projections data and fig

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,7 @@
 # setup ---------------------------------------------------------------------------------------
 
 library(tidyverse)
+library(readxl)
 library(here)
 
 # get st pete tide gauge data -----------------------------------------------------------------
@@ -15,3 +16,27 @@ tddat <- read.table('https://tidesandcurrents.noaa.gov/sltrends/data/8726520_mea
   )
 
 save(tddat, file = here('data/tddat.RData'))
+
+# SLR scenarios for St Pete -------------------------------------------------------------------
+
+# https://sealevel.nasa.gov/task-force-scenario-tool?psmsl_id=520
+download.file('https://d3qt3aobtsas2h.cloudfront.net/edge/ws/search/projection?psmsl_id=520&format=csv&task_force=true', 
+              destfile = here('data-raw/sl_taskforce_scenarios_psmls_id_520.xlsx'), 
+              method = 'curl')
+
+# units in mm, convert to feet
+# select low, intermediate, high scenario
+slrdat <- read_excel(here('data-raw/sl_taskforce_scenarios_psmls_id_520.xlsx'), sheet = 'Total') |> 
+  filter(scenario %in% c('Low', 'Int', 'High')) |> 
+  filter(quantile == 50) |> 
+  select(scenario, `2020`:`2100`) |> 
+  pivot_longer(cols = `2020`:`2100`, names_to = 'year', values_to = 'slr_mm') |>
+  mutate(
+    year = as.numeric(year),
+    slr_ft = slr_mm / 304.8, # convert mm to feet
+    scenario = factor(scenario, levels = c('Low', 'Int', 'High'), 
+                      labels = c('NOAA Low', 'NOAA Intermediate', 'NOAA High'))
+  ) 
+
+save(slrdat, file = here('data/slrdat.RData'))
+
--- R/fig.R
+++ R/fig.R
@@ -16,7 +16,8 @@ p <- tddat |>
   geom_line(color = 'deepskyblue') +
   labs(
     x = NULL,
-    y = 'Feet (MSL)'
+    y = 'Feet (MSL)', 
+    caption = 'Source: https://tidesandcurrents.noaa.gov/sltrends/data/8726520_meantrend.txt'
   ) +
   scale_y_continuous(breaks = seq(-1, 1, by = 0.5), limits = c(-1, 1)) +
   scale_x_date(breaks = brks, date_labels = '%Y') +
@@ -30,3 +31,37 @@ p <- tddat |>
   png(here('fig/tddat.png'), height = 3, width = 8, res = 300, units = 'in')
   print(p)
   dev.off()
+
+# SLR scenarios for st pete -------------------------------------------------------------------
+
+load(file = here('data/slrdat.RData'))
+  
+p <- ggplot(slrdat, aes(x = year, y = slr_ft, color = scenario, group = scenario)) +
+  geom_line(size = 1) +
+  labs(
+    x = NULL,
+    y = 'RSLC in Feet (LMSL)', 
+    color = NULL,
+    title = 'Relative Sea Level Change Predictions',
+    subtitle = 'Gauge 8726520, St. Petersburg, FL' ,
+    caption = 'Source: https://sealevel.nasa.gov/task-force-scenario-tool?psmsl_id=520'
+  ) +
+  scale_color_manual(
+    values = c('NOAA Low' = 'deepskyblue', 
+               'NOAA Intermediate' = 'orange', 
+               'NOAA High' = 'red')
+  ) +
+  scale_y_continuous(breaks = seq(0, 8, by = 1), limits = c(0, 8)) +
+  scale_x_continuous(breaks = seq(2020, 2100, by = 10)) +
+  theme_minimal() + 
+  theme(
+    panel.grid.minor = element_blank(), 
+    panel.grid.major.x = element_blank(), 
+    axis.ticks = element_line(), 
+    legend.position = 'bottom'
+  )
+  
+png(here('fig/slrdat.png'), height = 4, width = 8, res = 300, units = 'in')
+print(p)
+dev.off()
+  
