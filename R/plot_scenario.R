690204600ec1b149db4e87cf648b0e7cc6665684
fawda123 <mbafs2012@gmail.com>
2025-05-23 08:39:27 -0400

    finish vignettes, modify plot args

--- R/plot_scenario.R
+++ R/plot_scenario.R
@@ -5,12 +5,10 @@
 #' @param units character, units for the y-axis. Default is `'ft'`. Options are `'ft'` and `'m'`.
 #' @param linewidth numeric, line width. Default is `1`.
 #' @param caption logical, add caption with source. Default is `TRUE`.
-#' @param title logical, add title. Default is `TRUE`.
-#' @param subtitle character, subtitle for the plot. Default is `'Gauge 8726520, St. Petersburg, FL'`. Use `NULL` to omit.
 #' @param xrng numeric, x-axis range. Default is `c(2020, 2100)`.
-#' @param xbrk numeric, x-axis breaks. Default is `10`.
-#' @param yrng numeric, y-axis range. Default is `c(0, 6)`.
-#' @param ybrk numeric, y-axis breaks. Default is `1`.
+#' @param xbrk numeric, number of x-axis breaks. Default is `10`.
+#' @param yrng numeric, y-axis range as two values. Default is `NULL`, which uses the range of the data..
+#' @param ybrk numeric, number of y-axis breaks. Default is `7`.
 #'
 #' @returns a ggplot object
 #' @export
@@ -19,8 +17,7 @@
 #' dat <- get_scenario()
 #' plot_scenario(dat)
 plot_scenario <- function(dat, cols = c('deepskyblue', 'orange', 'red'), units = 'ft', linewidth = 1, 
-                          caption = TRUE, title = TRUE, subtitle = 'Gauge 8726520, St. Petersburg, FL',
-                          xrng = c(2020, 2100), xbrk = 10, yrng = c(0, 6), ybrk = 1){
+                          caption = TRUE, xrng = c(2020, 2100), xbrk = 10, yrng = NULL, ybrk = 7){
   
   units <- match.arg(units, c('ft', 'm'))
   
@@ -35,18 +32,23 @@ plot_scenario <- function(dat, cols = c('deepskyblue', 'orange', 'red'), units =
   toplo <- dat |> 
     dplyr::rename(
       yvl = !!colnm
-    )  
+    ) |> 
+    dplyr::filter(
+      year >= xrng[1] & year <= xrng[2]
+    )
 
+  if(is.null(yrng))
+    yrng <- c(0, max(toplo$yvl, na.rm = TRUE))
+  
   p <- ggplot2::ggplot(toplo, ggplot2::aes(x = year, y = yvl, color = scenario, group = scenario)) +
     ggplot2::geom_line(linewidth = linewidth) +
     ggplot2::labs(
       x = NULL,
       y = ylab,
-      color = NULL,
-      subtitle = subtitle
+      color = NULL
     ) +
     ggplot2::scale_color_manual(values = colval) +
-    ggplot2::scale_y_continuous(breaks = seq(yrng[1], yrng[2], by = ybrk), limits = yrng) +
+    ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(n = ybrk), limits = yrng) +
     ggplot2::scale_x_continuous(breaks = scales::pretty_breaks(n = xbrk), limits = xrng) +
     ggplot2::theme_minimal() +
     ggplot2::theme(
@@ -60,11 +62,6 @@ plot_scenario <- function(dat, cols = c('deepskyblue', 'orange', 'red'), units =
     cp <- paste0('Source: https://sealevel.nasa.gov/task-force-scenario-tool?psmsl_id=', unique(dat$id))
     p <- p + 
       ggplot2::labs(caption = cp) 
-  }
-  
-  if(title){
-    p <- p + 
-      ggplot2::labs(title = 'Relative Sea Level Change Predictions')
   }
   
   return(p)
--- R/plot_sealevel.R
+++ R/plot_sealevel.R
@@ -4,10 +4,10 @@
 #' @param col character, color for the line. Default is `'deepskyblue'`.
 #' @param units character, units for the y-axis. Default is `'ft'`. Options are `'ft'` and `'m'`.
 #' @param caption logical, add caption with source. Default is `TRUE`.
-#' @param xrng Date, x-axis range. Default is `NULL`, which uses the range of the data.
-#' @param xbrk numeric, x-axis breaks. Default is `10`.
-#' @param yrng numeric, y-axis range. Default is `c(-1, 1)`.
-#' @param ybrk numeric, y-axis breaks. Default is `0.5`.
+#' @param xrng Date, x-axis range as two values. Default is `NULL`, which uses the range of the data.
+#' @param xbrk numeric, number of x-axis breaks. Default is `10`.
+#' @param yrng numeric, y-axis range as two values. Default is `NULL`, which uses the range of the data.
+#' @param ybrk numeric, number of y-axis breaks. Default is `3`.
 #'
 #' @returns a ggplot object
 #' @export
@@ -15,7 +15,7 @@
 #' @examples
 #' dat <- get_sealevel()
 #' plot_sealevel(dat)
-plot_sealevel <- function(dat, col = 'deepskyblue', units = 'ft', caption = TRUE, xrng = NULL, xbrk = 10, yrng = c(-1, 1), ybrk = 0.5) {
+plot_sealevel <- function(dat, col = 'deepskyblue', units = 'ft', caption = TRUE, xrng = NULL, xbrk = 10, yrng = NULL, ybrk = 3) {
   
   units <- match.arg(units, c('ft', 'm'))
   
@@ -24,24 +24,29 @@ plot_sealevel <- function(dat, col = 'deepskyblue', units = 'ft', caption = TRUE
   }
   if(!is.null(xrng))
     stopifnot(inherits(xrng, 'Date'))
-  
+
   ylab <- ifelse(units == 'ft', 'Feet (MSL)', 'Meters (MSL)')
-  ybrks <- seq(yrng[1], yrng[2], by = ybrk) 
   
   colnm <- names(dat)[grepl(paste0('msl_', units), names(dat))]
   
   toplo <- dat |> 
     dplyr::rename(
       yvl = !!colnm
-    )  
+    ) |> 
+    dplyr::filter(
+      date >= xrng[1] & date <= xrng[2]
+    )
   
+  if(is.null(yrng))
+    yrng <- range(toplo$yvl, na.rm = TRUE)
+
   p <- ggplot2::ggplot(toplo, ggplot2::aes(x = date, y = yvl)) +
     ggplot2::geom_line(color = col) +
     ggplot2::labs(
       x = NULL,
       y = ylab
     ) +
-    ggplot2::scale_y_continuous(breaks = ybrks, limits = yrng) +
+    ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(n = ybrk), limits = yrng) +
     ggplot2::scale_x_date(breaks = scales::pretty_breaks(n = xbrk), limits = xrng) +
     ggplot2::theme_minimal() +
     ggplot2::theme(
